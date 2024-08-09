8776289911cb8814b90918e0bb0a43ebb394d61b
Chico-Carneiro <122817973+Chico-Carneiro@users.noreply.github.com>
2024-08-09 15:35:23 +0100

    2024/08/09
    It took me way less than a hour and it is beating more than 95% (we never know for sure) both in time and memory.
    However, I was unaware I would take this important hint while reading the Discussion section: there are only 8 magic number configurations.
    I would probably need between more 20 minutes to another full hour to realize that, depending on how lucky I would be in my own "research".
    On the other hand, I think the Description was poorly written, I interpreted it the wrong way, and reading the Discussion helped me prevent wasting lots of time.

---
+++ 840 Magic Squares In Grid/840.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var numMagicSquaresInside = function(grid) {
+    const cols = grid[0].length;
+    const rows = grid.length;
+    if (cols < 3 || rows < 3) return 0;
+
+    const mapper = {
+        "492357816": true,
+        "294753618": true,
+        "276951438": true,
+        "672159834": true,
+        "618753294": true,
+        "816357492": true,
+        "834159672": true,
+        "438951276": true,
+    }
+    let count = 0;
+    for (let i = 1; i < rows - 1; i++) {
+        for (let j = 1; j < cols - 1; j++) {
+            if (grid[i][j] === 5) {
+                const str1 = ''+grid[i-1][j-1]+grid[i-1][j]+grid[i-1][j+1]+grid[i][j-1]+grid[i][j]+grid[i][j+1]+grid[i+1][j-1]+grid[i+1][j]+grid[i+1][j+1];
+                        if (str1 in mapper) {
+                            count++;
+                            j++;
+                        }
+
+            }
+        }
+    }
+    return count;
+};
