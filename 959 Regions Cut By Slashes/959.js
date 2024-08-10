e738b7c93a363752669fbedac1f90e7e184bcf3c
Chico-Carneiro <122817973+Chico-Carneiro@users.noreply.github.com>
2024-08-10 22:16:53 +0100

    2024/08/10
    I didn't even try today.

---
+++ 959 Regions Cut By Slashes/959.js
@@ -0,0 +1,14 @@
+/*
+  I didn't even try today. I didn't have much time and after reading the Problem description
+I had doubts about what was being asked.
+In the discussion, many said this wasn't a Medium problem but a Hard one.
+Therefore I decided it would be better to find time to read the Editorial.
+As I read throught the Editorial, it still "blew my mind", mainly because of the set of skills
+one needs to have in order to approach such a problem.
+The major point I would like to take from this particular problem is how much "bolder" I need
+to be from the start when approaching a problem like this, so I can intuitively abstract from the given
+framing of the problem - including the given wording, data structures, examples, etc. - much sooner.
+I am very creative, but it is true that I tend to approach these problems conservatively, trying to
+use the given settings as much as possible, instead of going immediatelly for a completely reframing
+of the concepts.
+*/
