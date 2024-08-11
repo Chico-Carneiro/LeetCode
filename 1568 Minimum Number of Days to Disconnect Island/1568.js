196e68d089a0768ce580e7933fd40ae22dfebd9b
Chico-Carneiro <122817973+Chico-Carneiro@users.noreply.github.com>
2024-08-11 17:39:59 +0100

    2024/08/11
    Today I don't have time to dive deeper into this problem.
    I just outlined the overall idea, I'll be reading the Editorial later.
    The major part of the solution is about parsing the matrix and considering the eventual local edge cases that an island must have so 1 removal is enough.
    For example, 3 linear consecutive blocks allows just 1 removal.
    But something like
    [ ][ ][ ] 
    [ ]    [ ]
    [ ][ ][ ]    needs 2.
    And
    [ ][ ][ ] 
    [ ]    [ ][ ]
    [ ][ ][ ]    needs only 1 again, which hints to the pattern o8o, 
    
    So I would analyse these patterns further.

---
+++ 1568 Minimum Number of Days to Disconnect Island/1568.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var minDays = function(grid) {
+    // output is either 0, 1 or 2.
+
+    // check if there's exactly 1 island
+      // break if there's 2, return 0
+    // if there's exactly 1 island
+      // if its topology allows for 1 removal
+        // return 1
+      // if not, return 2
+
+      
+};
