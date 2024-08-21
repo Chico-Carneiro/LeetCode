d9000eff2cbcc1467f04d304487cc03a71651ff1
Chico-Carneiro <122817973+Chico-Carneiro@users.noreply.github.com>
2024-08-21 17:58:58 +0100

    2024/08/21
    I looked at the Problem, read the Discussion.
    I don't have much time today, this is a hard problem.
    I don't have any solid idea.
    I feel that thinking about counting blocks of similar letters would be moving towards a strategy.
    Ex: "aabbccbbffggaahhaallaa" => a: 4, b: 2, c: 1, f: 1, g: 1, h: 1, l: 1 
    After spending so much time yesterday I will have to pass on this one.
    I'll read the Editorial and hopefully learn something.

---
+++ 664 Strange Printer/644.js
@@ -0,0 +1,9 @@
+/*
+I looked at the Problem, read the Discussion.
+I don't have much time today, this is a hard problem.
+I don't have any solid idea.
+I feel that thinking about counting blocks of similar letters would be moving towards a strategy.
+Ex: "aabbccbbffggaahhaallaa" => a: 4, b: 2, c: 1, f: 1, g: 1, h: 1, l: 1 
+After spending so much time yesterday I will have to pass on this one.
+I'll read the Editorial and hopefully learn something.
+*/
