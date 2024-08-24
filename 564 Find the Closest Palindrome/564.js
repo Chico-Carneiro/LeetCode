90c9d22dd93cc555a1bb79df6a16ba4ec6d7991d
Chico-Carneiro <122817973+Chico-Carneiro@users.noreply.github.com>
2024-08-25 00:42:02 +0100

    2024/08/24
    I didn't have much time, nor patience tbh.
    After a brief analysis, I got the overall idea, although I couldn't be sure without further exploration
    . Find the midpoint,
    . mirror the number,
    . check for problems in the digit left to midpoint,
    . check for problems in strings like "100", "1000", "10000", ...
    I didn't get into the details, nor did I was sure there wouldn't exist any other edge cases.
    
    On top of that, I was convinced I would need to implement string manipulation versions to perform comparisons,
    which was going to take some time.

---
+++ 564 Find the Closest Palindrome/564.js
@@ -0,0 +1,18 @@
+/*
+I didn't have much time, nor patience tbh.
+After a brief analysis, I got the overall idea, although I couldn't be sure without further exploration
+. Find the midpoint,
+. mirror the number,
+. check for problems in the digit left to midpoint,
+. check for problems in strings like "100", "1000", "10000", ...
+I didn't get into the details, nor did I was sure there wouldn't exist any other edge cases.
+
+On top of that, I was convinced I would need to implement string manipulation versions to perform comparisons,
+which was going to take some time.
+
+So I decided to just read the Editorial.
+I wasn't very far from approach 1.
+I can't understand approach 2, which uses binary search. I guess I'll try to read it again.
+
+
+*/
