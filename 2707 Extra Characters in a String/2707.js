82928f9584e1cd55a16dd47fff487398bc0863aa
Chico-Carneiro <122817973+Chico-Carneiro@users.noreply.github.com>
2024-09-23 03:50:23 +0100

    2024/09/23
    Dynamic Programming keeps being challenging.
    I knew I would need kind of a tree and use some sort of memoization.
    Yet, after a full hour, I couldn't come up with anything concrete.
    After giving up, I realized I should have at least checked the hints, which I didn't. (I just checked, I'm not sure it would have helped haha)
    Tough times. It will get better soon enough.

---
+++ 2707 Extra Characters in a String/2707.js
@@ -0,0 +1,33 @@
+/**
+ * @param {string} s
+ * @param {string[]} dictionary
+ * @return {number}
+ */
+/*This code isn't mine. It's given in the Editorial section.*/
+var minExtraChar = function(s, dictionary) {
+    const n = s.length;
+    const dictionarySet = new Set(dictionary);
+    const memo = new Map();
+
+    const dp = function(start) {
+        if (start === n) {
+            return 0;
+        }
+        if (memo.has(start)) {
+            return memo.get(start);
+        }
+        // To count this character as a left over character 
+        // move to index 'start + 1'
+        let ans = dp(start + 1) + 1;
+        for (let end = start; end < n; end++) {
+            const curr = s.slice(start, end + 1);
+            if (dictionarySet.has(curr)) {
+                ans = Math.min(ans, dp(end + 1));
+            }
+        }
+        memo.set(start, ans);
+        return ans
+    };
+
+    return dp(0);
+};
