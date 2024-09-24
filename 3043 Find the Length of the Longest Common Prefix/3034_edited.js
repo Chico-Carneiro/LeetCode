a48566ff0c662320d6c6e3445100f7eec31340e4
Chico-Carneiro <122817973+Chico-Carneiro@users.noreply.github.com>
2024-09-24 22:30:49 +0100

    2024/09/24
    Just realized I was doing repeated work.
    Not this beats 75% in terms of time, still below 10% on space.
    The space can be improved if I use Maps / HashMaps instead of Arrays.
    Now to the Editorial.

---
+++ 3043 Find the Length of the Longest Common Prefix/3034_edited.js
@@ -0,0 +1,55 @@
+/**
+ * @param {number[]} arr1
+ * @param {number[]} arr2
+ * @return {number}
+ */
+var longestCommonPrefix = function (arr1, arr2) {
+    const t1 = buildTrie(arr1);
+    //const t2 = buildTrie(arr2);
+
+    let max = 0;
+/*
+    for (let word1 of arr1) {
+        max = Math.max(max, findDepth(word1, t2));
+    }
+*/
+    for (let word2 of arr2) {
+        max = Math.max(max, findDepth(word2.toString(), t1));
+    }
+
+    return max;
+
+};
+
+function buildTrie(arr) {
+    const trie = new Nodi();
+    for (let [index, num] of arr.entries()) {
+        const sNum = num.toString();
+        arr[index] = sNum;
+        let curr = trie;
+        for (let i = 0; i < sNum.length; i++) {
+            if (curr.alpha[sNum[i]] === null) {
+                curr.alpha[sNum[i]] = new Nodi();
+            }
+            curr = curr.alpha[sNum[i]];
+        }
+    }
+    return trie;
+}
+
+function Nodi() {
+    this.alpha = new Array(10).fill(null);
+}
+
+function findDepth(word, trie) {
+    let count = 0;
+    let curr = trie;
+    for (let i = 0; i < word.length && curr !== undefined; i++) {
+        if (curr.alpha[word[i]] === null) {
+            return count;
+        };
+        count++;
+        curr = curr.alpha[word[i]];
+    }
+    return count;
+}
