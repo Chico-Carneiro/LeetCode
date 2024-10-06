4805506957d0c01673d1e6ce4e4641215223b8bb
Chico-Carneiro <122817973+Chico-Carneiro@users.noreply.github.com>
2024-10-06 17:44:56 +0100

    2024/10/06
    Today I have better things to do, so after a brief thought I decided to move on.
    This was provided by LeetCode, not my implementation.
    I'll read the Editorial later.

---
+++ 1813 Sentence Similarity III/1813.py
@@ -0,0 +1,17 @@
+# Today I have better things to do, so after a brief
+# thought I decided to move on
+# This was provided by LeetCode, not my implementation
+
+class Solution:
+    def areSentencesSimilar(self, s1: str, s2: str) -> bool:
+        deque1 = deque(s1.split())
+        deque2 = deque(s2.split())
+        # Compare the prefixes or beginning of the strings.
+        while deque1 and deque2 and deque1[0] == deque2[0]:
+            deque1.popleft()
+            deque2.popleft()
+        # Compare the suffixes or ending of the strings.
+        while deque1 and deque2 and deque1[-1] == deque2[-1]:
+            deque1.pop()
+            deque2.pop()
+        return not deque1 or not deque2
