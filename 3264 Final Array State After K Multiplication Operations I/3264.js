292630e68a7f7df04888711d659e815c4408cb35
Chico-Carneiro <122817973+Chico-Carneiro@users.noreply.github.com>
2024-12-16 02:30:02 +0000

    2024/12/16

--- 3264 Final Array State After K Multiplication Operations I/3264.js
+++ 3264 Final Array State After K Multiplication Operations I/3264.js
@@ -9,7 +9,6 @@ var getFinalState = function (nums, k, multiplier) {
     if (nums.length == 1) return [nums[0] * multiplier ** k];
 
     const arr = Array(nums.length);
-
     for (let i = 0; i < nums.length; i++) {
         arr[i] = [nums[i], i];
     }
