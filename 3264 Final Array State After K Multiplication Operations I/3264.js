5705ae68d827c90d0fb8ad7fab753ffdca9d8a52
Chico-Carneiro <122817973+Chico-Carneiro@users.noreply.github.com>
2024-12-16 02:16:27 +0000

    Create 3264.js

---
+++ 3264 Final Array State After K Multiplication Operations I/3264.js
@@ -0,0 +1,143 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @param {number} multiplier
+ * @return {number[]}
+ */
+var getFinalState = function (nums, k, multiplier) {
+    if (multiplier == 1) return nums;
+    if (nums.length == 1) return [nums[0] * multiplier ** k];
+
+    const arr = Array(nums.length);
+
+    for (let i = 0; i < nums.length; i++) {
+        arr[i] = [nums[i], i];
+    }
+
+    const customComparator = (a, b) => {
+        if (a[0] == b[0]) return a[1] - b[1];
+        else return a[0] - b[0];
+    }
+    const heap = Heap.heapify(arr, customComparator);
+
+    while (k > 0) {
+        const min = heap.pop();
+        const num = min[0] * multiplier;
+        nums[min[1]] = num;
+        min[0] = num;
+        heap.push(min);
+        k--;
+    }
+    return nums;
+};
+
+class Heap {
+    constructor(comparator = (a, b) => a - b) {
+        this.heap = [];
+        this.comparator = comparator;
+    }
+    size() {
+        return this.heap.length;
+    }
+    peek() {
+        return this.heap[0];
+    }
+    // Push a new element into the heap
+    push(value) {
+        this.heap.push(value);
+        this.bubbleUp();
+    }
+
+    // Pop the smallest element from the heap
+    pop() {
+        if (this.heap.length === 0) return null;
+        const root = this.heap[0];
+        const last = this.heap.pop();
+        if (this.heap.length > 0) {
+            this.heap[0] = last;
+            this.bubbleDown();
+        }
+        return root;
+    }
+
+    // Push and pop in one operation
+    pushPop(value) {
+        if (this.heap.length === 0) {
+            //this.push(value);
+            return value;
+        }
+        if (this.comparator(value, this.heap[0]) > 0) {
+            const root = this.heap[0];
+            this.heap[0] = value;
+            this.bubbleDown();
+            return root;
+        }
+        return value;
+    }
+
+    // Bubble up the last element to maintain heap property
+    bubbleUp() {
+        let index = this.heap.length - 1;
+        while (index > 0) {
+            const parentIndex = Math.floor((index - 1) / 2);
+            if (this.comparator(this.heap[index], this.heap[parentIndex]) >= 0) break;
+            [this.heap[index], this.heap[parentIndex]] = [this.heap[parentIndex], this.heap[index]];
+            index = parentIndex;
+        }
+    }
+
+    // Bubble down the root element to maintain heap property
+    bubbleDown() {
+        let index = 0;
+        const length = this.heap.length;
+        while (true) {
+            const leftChildIndex = 2 * index + 1;
+            const rightChildIndex = 2 * index + 2;
+            let smallestIndex = index;
+
+            if (leftChildIndex < length && this.comparator(this.heap[leftChildIndex], this.heap[smallestIndex]) < 0) {
+                smallestIndex = leftChildIndex;
+            }
+            if (rightChildIndex < length && this.comparator(this.heap[rightChildIndex], this.heap[smallestIndex]) < 0) {
+                smallestIndex = rightChildIndex;
+            }
+            if (smallestIndex === index) break;
+            [this.heap[index], this.heap[smallestIndex]] = [this.heap[smallestIndex], this.heap[index]];
+            index = smallestIndex;
+        }
+    }
+
+    // Create a heap from an array in O(n) time
+    static heapify(array, comparator) {
+        const heap = new Heap(comparator);
+        heap.heap = array.slice(); // Copy the array
+        const start = Math.floor(heap.heap.length / 2) - 1; // Last non-leaf node
+        for (let i = start; i >= 0; i--) {
+            heap.bubbleDownFromIndex(i);
+        }
+        return heap;
+    }
+
+    // Bubble down from a specific index
+    bubbleDownFromIndex(index) {
+        const length = this.heap.length;
+        while (true) {
+            const leftChildIndex = 2 * index + 1;
+            const rightChildIndex = 2 * index + 2;
+            let smallestIndex = index;
+
+            if (leftChildIndex < length && this.comparator(this.heap[leftChildIndex], this.heap[smallestIndex]) < 0) {
+                smallestIndex = leftChildIndex;
+            }
+            if (rightChildIndex < length && this.comparator(this.heap[rightChildIndex], this.heap[smallestIndex]) < 0) {
+                smallestIndex = rightChildIndex;
+            }
+            if (smallestIndex === index) break;
+            [this.heap[index], this.heap[smallestIndex]] = [this.heap[smallestIndex], this.heap[index]];
+            index = smallestIndex;
+        }
+    }
+
+}
+
+
